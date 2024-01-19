57a579ea0ae53abca308edf0465c3adabe27533c
pablo <pablo@1DAM-20>
2024-01-19 13:49:08 +0100

    Commit triangulo2

--- src/Proyecto/AreaTriangulo.java
+++ src/Proyecto/AreaTriangulo.java
@@ -1,5 +1,25 @@
 package Proyecto;
 
-public class AreaTriangulo {
+	import java.util.Scanner;
 
-}
+	public class AreaTriangulo {
+		
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Ingrese el lado 1: ");
+	        int lado1 = scanner.nextInt();
+
+	        System.out.print("Ingrese el lado 2: ");
+	        int lado2 = scanner.nextInt();
+
+	        double area = calcularArea(lado1, lado2);
+
+	        System.out.println("El área del triángulo es: " + area);
+	    }
+
+	    public static double calcularArea(int lado1, int lado2) {
+	        double area = (lado1 * lado2) / 2.0;
+	        return area;
+	    }
+	}
